07e33b8a40d8b9aa3946943133a245b52218c853
Ghayyas <ghayyas@Razas-MacBook-Pro.local>
2016-06-23 18:23:11 +0500

    create events but not working properly

--- app/pages/create-event/create-event.ts
+++ app/pages/create-event/create-event.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {CreateListPeopleInvitePage} from '../create-list-people-invite/create-list-people-invite';
-
+import {Http, Headers } from '@angular/http';
 /*
   Generated class for the CreateEventPage page.
 
@@ -11,7 +11,32 @@ import {CreateListPeopleInvitePage} from '../create-list-people-invite/create-li
 @Component({
   templateUrl: 'build/pages/create-event/create-event.html',
 })
+
 export class CreateEventPage {
+    http: any;
+    parameters: {
+     name: string,
+     location: string,
+     start_date: string,
+     end_date: string,
+     type: boolean,
+     description : string,
+     photo:string
+}
    createListPeopleToInvite = CreateListPeopleInvitePage;
-  constructor(public nav: NavController) {}
+  constructor(public nav: NavController, http: Http) {
+    this.http = http;
+}
+
+  submit(parameters){
+    console.log('params',parameters)
+  //   var headers = new Headers();
+  //   var data  = "event-name" + parameters.name + "event-location"+ parameters.location + "event-start-date" + parameters.start_date + "event-end-date" + parameters.end_date + "event-image" + parameters.photo + "event-details" + parameters.description;
+  //  headers.append('Content-Type', 'application/x-www-form-urlencoded');
+  //   this.http.post('http://nameless-scrubland-35696.herokuapp.com/api/events/create',data,{headers:headers})
+  //   .subscribe(data => {
+  //     console.log('reciveing data',data);
+  //   })
+
+}
 }
