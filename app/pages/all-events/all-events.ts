bf3bca20c25a228ff61144138d44193a0a4baf9a
Ghayyas <ghayyas@Razas-MacBook-Pro.local>
2016-06-24 18:26:22 +0500

    events is created and viewing on all events but camera functionality is missing

--- app/pages/all-events/all-events.ts
+++ app/pages/all-events/all-events.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {EventDetailsPage} from '../event-details/event-details';
 import {AllEventFormPage} from '../all-event-form/all-event-form';
+import {Http, Headers } from '@angular/http';
+
 
 /*
   Generated class for the AllEventsPage page.
@@ -14,6 +16,44 @@ import {AllEventFormPage} from '../all-event-form/all-event-form';
 })
 export class AllEventsPage {
   eventDetailPage = EventDetailsPage;
+  event: [Object];
+  arr:[Object];
+  // ev:string
   alleventformscreen = AllEventFormPage;
-  constructor(public nav: NavController) {}
+  constructor(public nav: NavController, public http:Http) {
+    this.http = http;
+
+    let headers = new Headers();
+   headers.append('Content-Type', 'application/json');
+   let ecnobToken = window.localStorage.getItem('ecnob.token');
+   headers.append('Authorization', `Bearer ${ecnobToken}`)
+    this.http.get('http://nameless-scrubland-35696.herokuapp.com/api/events/get',{headers:headers})
+    .subscribe(
+      (data)=>{
+  // this.arr = [{"name":"value"},{"name":"hers"},{"name":"abcd"}]
+      
+          // let s = data;
+        this.event = data.json().events;
+        console.log('data',this.event);
+      
+
+      },
+      (err)=>{
+        console.log('err',err);
+        let str = JSON.parse(err._body);
+      // str = str.replace(/\\/g, '')
+      if(str.status_code == 422){
+        console.log('Please Fill all Required Fields');
+      }
+      console.log('status code',str.status_code)
+      console.log('error reciveing', str.message);
+      }
+    )
+  }
+  // getRandom(){
+
+  
+
+    // console.log('params',this.parameters);
+    
 }
--- app/pages/create-event/create-event.ts
+++ app/pages/create-event/create-event.ts
@@ -1,7 +1,12 @@
 import {Component} from "@angular/core";
-import {NavController,Page} from 'ionic-angular';
+import {NavController,Page,ActionSheet} from 'ionic-angular';
+import {RADIO_GROUP_DIRECTIVES} from "ng2-radio-group";
+import {Camera} from 'ionic-native';
+import {NgZone} from "@angular/core";
+
 import {CreateListPeopleInvitePage} from '../create-list-people-invite/create-list-people-invite';
 import {Http, Headers } from '@angular/http';
+
 // import {RadioControlValueAccessor} from "./radio_value_accessor";
 
 /*
@@ -11,32 +16,101 @@ import {Http, Headers } from '@angular/http';
   Ionic pages and navigation.
 */
 
+declare var navigator: any;
+
 
 @Component({
   templateUrl: 'build/pages/create-event/create-event.html',
+    directives: [RADIO_GROUP_DIRECTIVES]
+
 })
 
 export class CreateEventPage {
-      //  http: any;
-      
-       parameters = {}
-
-
+    
+    parameters = {
+      photo : ""
+    }
+    // base64Image: string;
+    zone: any;
+    
    createListPeopleToInvite = CreateListPeopleInvitePage;
   constructor(public nav: NavController, private http:Http) {
     this.http = http;
-    
-}
+    // this.image = "https://placehold.it/150x150";
+        this.zone = new NgZone({enableLongStackTrace: false});
+  }
+  presentActionSheet() {
+  let actionSheet = ActionSheet.create({
+    title: 'Select from Camera',
+    buttons: [
+      {
+        text: 'Take Picture form Camera',
+        role: 'destructive',
+        handler: () => {
+          Camera.getPicture({
+            quality : 75,
+            destinationType : navigator.camera.DestinationType.DATA_URL,
+            sourceType : navigator.camera.PictureSourceType.CAMERA,
+            allowEdit : true,
+            encodingType: navigator.camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            saveToPhotoAlbum: false
+        }).then(imageData => {
+            this.zone.run(() => {
+                this.parameters.photo = "data:image/jpeg;base64," + imageData;
+            });
+        }, error => {
+            console.log("ERROR -> " + JSON.stringify(error));
+        });
+          // console.log('Destructive clicked');
+        }
+      },
+      {
+        text: 'Upload from Gallery',
+        handler: () => {
+          console.log('Archive clicked');
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+    ]
+  });
 
-  submit(){
-    console.log('params',this.parameters);
-  //   var headers = new Headers();
-  //   var data  = "event-name" + parameters.name + "event-location"+ parameters.location + "event-start-date" + parameters.start_date + "event-end-date" + parameters.end_date + "event-image" + parameters.photo + "event-details" + parameters.description;
-  //  headers.append('Content-Type', 'application/x-www-form-urlencoded');
-  //   this.http.post('http://nameless-scrubland-35696.herokuapp.com/api/events/create',data,{headers:headers})
-  //   .subscribe(data => {
-  //     console.log('reciveing data',data);
-  //   })
+  this.nav.present(actionSheet);
+  }
+
+  takePicture() {
+        
+    }
+
+  submit(parameters){
+    // console.log('params',this.parameters);
+    var headers = new Headers();
+    var data  = this.parameters;
+   headers.append('Content-Type', 'application/json');
+   let ecnobToken = window.localStorage.getItem('ecnob.token');
+   headers.append('Authorization', `Bearer ${ecnobToken}`)
+    this.http.post('http://nameless-scrubland-35696.herokuapp.com/api/events/create',data,{headers:headers})
+    .subscribe(
+      (data) => {
+      console.log('reciveing data',data);
+    },
+    (err) =>{
+      let str = JSON.parse(err._body);
+      // str = str.replace(/\\/g, '')
+      if(str.status_code == 422){
+        console.log('Please Fill all Required Fields');
+      }
+      console.log('status code',str.status_code)
+      console.log('error reciveing', str.message);
+    }
+    )
 
 }
 }
