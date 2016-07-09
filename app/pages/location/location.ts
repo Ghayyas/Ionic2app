6880da1d4f0ed10ddcb610abc9cf31f78bed54d5
Ghayyas Mubashir <ghayyasmubashir@gmail.com>
2016-07-09 18:36:11 +0500

    location page is complete functionality is missing

--- app/pages/location/location.ts
+++ app/pages/location/location.ts
@@ -6,18 +6,15 @@ import {DataService} from '../../service/dataService/dataService';
 
 declare var google:any;
 declare var load:any;    
+declare var $:any;
 
  @Page({
   templateUrl: 'build/pages/location/location.html',
-  providers: [DataService],
-  inputs: ['inputValue'],    
-  outputs: ['inputValueChange']
+  providers: [DataService]
 })
 
 export class locationPage{
     public map:any;
-    inputValue: boolean;
-    inputValueChange: EventEmitter<any> = new EventEmitter();
 
     public cityCircle;   //Circle Map
     public myRadius;      ///Radius of the Map
@@ -40,6 +37,9 @@ export class locationPage{
           center: myLatLng
         });
 
+
+        
+        
         var marker = new google.maps.Marker({
           position: myLatLng,
           map: map,
@@ -75,6 +75,64 @@ export class locationPage{
   });
   this.cityCircle.bindTo('center', marker, 'position');
   
+        // Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+        
+    var input = document.getElementById('pac-input');         
+    var autocomplete = new google.maps.places.Autocomplete(input, {
+        types: ["geocode"]
+    });          
+    
+    autocomplete.bindTo('bounds', map); 
+    var infowindow = new google.maps.InfoWindow(); 
+ 
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        infowindow.close();
+        var place = autocomplete.getPlace();
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  
+        }
+        
+        moveMarker(place.name, place.geometry.location);
+    });  
+     $("input").focusin(function () {
+        $(document).keypress(function (e) {
+            if (e.which == 13) {
+                infowindow.close();
+                var firstResult = $(".pac-container .pac-item:first").text();
+                
+                var geocoder = new google.maps.Geocoder();
+                geocoder.geocode({"address":firstResult }, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        var lat = results[0].geometry.location.lat(),
+                            lng = results[0].geometry.location.lng(),
+                            placeName = results[0].address_components[0].long_name,
+                            latlng = new google.maps.LatLng(lat, lng);
+                        
+                        moveMarker(placeName, latlng);
+                        $("input").val(firstResult);
+                    }
+                });
+            }
+        });
+    });
+     
+     function moveMarker(placeName, latlng){
+        // marker.setIcon(image);
+        marker.setPosition(latlng);
+        infowindow.setContent(placeName);
+        infowindow.open(map, marker);
+     }
 
       }
       
